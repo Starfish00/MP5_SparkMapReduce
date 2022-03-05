61ccd547bd7684f0c13321a942f50229996edceb
Starfish00 <rena900@yahoo.com>
2022-03-04 23:13:04 -0800

    Add files via upload

--- PythonTemplate/TitleCountSpark.py
+++ PythonTemplate/TitleCountSpark.py
@@ -8,11 +8,16 @@ from pyspark import SparkConf, SparkContext
 stopWordsPath = sys.argv[1]
 delimitersPath = sys.argv[2]
 
+stopWordsList = [];
+
 with open(stopWordsPath) as f:
 	#TODO
+    for line in f:
+        stopWordsList.append(line.strip())
 
 with open(delimitersPath) as f:
     #TODO
+    delimiters = f.read()
 
 conf = SparkConf().setMaster("local").setAppName("TitleCount")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
@@ -21,10 +26,24 @@ sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[3], 1)
 
 #TODO
+def preprocess(line):
+    for d in delimiters:
+        line = line.strip().replace(d, " ").lower()
+    return line
+
+words = lines.map(preprocess).flatMap(lambda line: line.split()) # to filter by stopWords .filter(lambda word: word not in stopWordsList)
+
+counts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+sorted_counts = sorted(counts, key=lambda word_count: word_count[1], reverse=True)
+
+top_counts = sorted(sorted_counts[:9], key=lambda word_count: word_count[0])
 
 outputFile = open(sys.argv[4],"w")
 
 #TODO
 #write results to output file. Foramt for each line: (line +"\n")
+for tc in top_counts:
+    f.write("%s\t%s\n" % tc)
 
 sc.stop()
